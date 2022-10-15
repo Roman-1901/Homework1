4000d8eb84fe2b271cf15060966730a80175adb4
John Rey <adioskid@mail.ru>
2022-10-15 13:37:12 +0300

    Cделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Здорова, Женек!!!");
+        JFrame jf = new JFrame("Window");
+        jf.setLayout(new FlowLayout());
+
+        jf.setSize(300, 200);
+        JLabel lb = new JLabel("Рома, не Димонь=)))");
+        jf.add(lb);
+        jf.setBounds(1030,450,500,400);
+
+        jf.setVisible(true);
+        lb.setVisible(true);
+
+
     }
 }
