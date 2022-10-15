a5f322efde45a15c2d9270fd9f52f306ae982931
Roman-1901 <roman-1901@mail.ru>
2022-10-15 15:19:59 +0300

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,6 @@
-import javax.swing.*;
-import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame jf = new JFrame("Window");
-        jf.setLayout(new FlowLayout());
-
-        jf.setSize(300, 200);
-        JLabel lb = new JLabel("Рома, не Димонь=)))");
-        jf.add(lb);
-        jf.setBounds(1030,450,500,400);
-        jf.setVisible(true);
-        lb.setVisible(true);
-
+        System.out.println("Привет, мир!");
     }
 }
